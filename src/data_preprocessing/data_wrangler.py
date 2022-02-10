f22dd8f5dc45c293fe29a690ac5a46db6cd6e64c
Karthik-VG <karthikvg.engineer@gmail.com>
2022-02-10 10:59:25 +0530

    update in data wrangler

--- src/data_preprocessing/data_wrangler.py
+++ src/data_preprocessing/data_wrangler.py
@@ -7,5 +7,6 @@ class data_wrangler:
 
     def remove_duplicates(self,raw_df):
         df_dup_removed=raw_df.remove_duplicates()
+        df_dup_removed=df_dup_removed.reindex(index=range(0,457620))
         return df_dup_removed
 
